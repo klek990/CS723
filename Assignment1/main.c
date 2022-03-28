5f899b0413954ed3a55c9365e55ffcc409068f1e
klek990 <klek990@aucklanduni.ac.nz>
2022-03-28 16:23:37 +1300

    added task allocation

---
+++ Assignment1/main.c
@@ -0,0 +1,14 @@
+/* Standard includes. */
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Scheduler includes. */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+int main(void)
+{
+    for(;;);
+}
