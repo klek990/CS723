b92f707862042df2f5fff4d1637fc95342570593
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2022-04-19 15:55:07 +1200

    Created System State Queue
    - Creates SystemStateQueue and prints debug message
    
    -Sends "2" to systemStateQueue when button ISR triggered.

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -22,6 +22,13 @@
 #define mainREG_TEST_PRIORITY (tskIDLE_PRIORITY + 1)
 #define SAMPLINGFREQUENCY 16e3
 
+#define SystemStateQueueSize 20
+
+#define normalState 0
+#define loadState 1
+#define managementState 2
+
+
 /* Semaphores */
 /* 0 = Normal, 1 = Load managing, 2 = Maintenance */
 SemaphoreHandle_t systemState;
@@ -29,6 +36,10 @@ SemaphoreHandle_t systemState;
 /* Frequency thresholds configured using keyboard */
 SemaphoreHandle_t lowerThreshold;
 SemaphoreHandle_t upperThreshold;
+
+//Define Queue for SystemState
+static QueueHandle_t xSystemStateQueue;
+
 
 static void processSignalTask(void *pvParameters);
 static void pollWallSwitchesTask(void *pvParameters);
@@ -94,6 +105,21 @@ void readKeyboardISR(void *context, alt_u32 id)
 	return;
 }
 
+void maintenanceStateISR(){
+	//Check which button was pressed
+	IOWR_ALTERA_AVALON_PIO_DATA(PUSH_BUTTON_PIO_BASE, );
+
+	/*When the correct push button is pressed, the number "2" representing must 
+	be appended to the  systemStateQueue */
+	int passToQueue = managementState;
+	if(xQueueSendFromISR(xSystemStateQueue, &passToQueue, NULL) == pdPASS){
+		"\nMaintenance State sucessfully sent to SystemStateQueue"
+	};
+
+	//Clear edge capture register
+	
+}
+
 /* This should be used to send the signal information to the other tasks */
 static void processSignalTask(void *pvParameters)
 {
@@ -152,23 +178,33 @@ int main(void)
 
 	alt_up_ps2_clear_fifo(keyboard);
 
-	/* Register FAU & keyboard */
+	/* Register FAU, keyboard and push button */
 	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, readFrequencyISR);
 	alt_irq_register(PS2_IRQ, keyboard, readKeyboardISR);
-
-	/* Register Button for Maintenance State */
-	alt_irq_register(PUSH_BUTTON_IRQ, 0, maintenanceStateIRQ);
-
-
+	alt_irq_register(PUSH_BUTTON_IRQ, 0, maintenanceStateISR);
 
 	/* Register keyboard interrupt */
 	IOWR_8DIRECT(PS2_BASE, 4, 1);
+
+	//ENABLE BUTTON INTERRUPTS
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_PIO_BASE, 0x4);
+
+	//CLEAR EDGE CAP REGISTER
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_PIO_BASE, 0);
 
 	/* The RegTest tasks as described at the top of this file. */
 	initCreateTasks();
 
 	/* Finally start the scheduler. */
 	vTaskStartScheduler();
+
+	xSystemStateQueue = xQueueCreate(SystemStateQueueSize, sizeof(int));
+	if(xSystemStateQueue == 0){
+		printf("\nUnable to Create Integer SystemStateQueue");
+	} 
+	else {
+		printf("\nSystemStateQueue Created Successfully")
+	}
 
 	/* Will only reach here if there is insufficient heap available to start
 	 the scheduler. */
