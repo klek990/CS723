5fac00930c4befe241eb1de62f96e5d54332647c
klek990 <klek990@aucklanduni.ac.nz>
2022-06-03 12:29:27 +1200

    Added drivingControl skeleton and regulate functions to C file

--- Assignment2/cruiseController.h
+++ Assignment2/cruiseController.h
@@ -2,3 +2,5 @@ int checkAccelPedal(float accel);
 int checkBrakePedal(float brake);
 int checkSpeedInRange(float speed);
 int checkSpeedIncrement(float cruiseSpeed, int quickAccel, int quickDecel);
+float saturateThrottle(float throttleIn, int* saturate);
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed);
--- Assignment2/cruiseController_data.c
+++ Assignment2/cruiseController_data.c
@@ -32,3 +32,53 @@ int checkSpeedIncrement(float cruiseSpeed, int quickAccel, int quickDecel)
         return (cruiseSpeed - 2.5 >= 30.0);
     }
 }
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: throttleIn - throttle input
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float saturateThrottle(float throttleIn, int* saturate)
+{
+	static const float THROTTLESATMAX = 45.0;
+	if (throttleIn > THROTTLESATMAX) {
+		*saturate = 1;
+		return THROTTLESATMAX;
+	}
+	else if (throttleIn < 0) {
+		*saturate = 1;
+		return 0;
+	}
+	else {
+		*saturate = 0;
+		return throttleIn;
+	}
+}
+
+/*
+DESCRIPTION: Saturate the throttle command to limit the acceleration.
+PARAMETERS: isGoingOn - true if the cruise control has just gone into the ON state 
+                        from another state; false otherwise
+            saturate - true if saturated, false otherwise
+RETURNS: throttle output (ThrottleCmd)
+*/
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed)
+{
+	static const float KP = 8.113;
+	static const float KI = 0.5;
+	static int saturate = 1;
+	static float iterm = 0;
+	
+	if (isGoingOn) {
+		iterm = 0;	// reset the integral action
+		saturate = 1;	
+	}
+	float error = cruiseSpeed - vehicleSpeed;
+	float proportionalAction = error * KP;
+	if (saturate)
+		error = 0;	// integral action is hold when command is saturated
+	iterm = iterm + error;
+	float integralAction = KI * iterm;
+	return saturateThrottle(proportionalAction + integralAction, &saturate);
+}
